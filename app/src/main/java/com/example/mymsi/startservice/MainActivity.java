49ea8ac7429df22a3024baa22ea3177735488195
JustHard <1411761044@qq.com>
2017-09-13 14:09:35 +0800

    Initial commit

---
+++ app/src/main/java/com/example/mymsi/startservice/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.mymsi.startservice;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btnStart = (Button)findViewById(R.id.buttonStart);
+        Button btnEnd = (Button)findViewById(R.id.buttonEnd);
+
+        btnStart.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,MyService.class);
+                intent.putExtra("num",10);
+                startService(intent);
+        }
+    });
+        btnEnd.setOnClickListener(new View.OnClickListener(){
+
+        @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,MyService.class);
+                stopService(intent);
+            }
+        });
+    }
+}
